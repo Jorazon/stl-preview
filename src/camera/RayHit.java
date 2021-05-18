07539b3d5ce56992b7c0079366452fb66fce254e
Jorazon <oskari.pahkala@gmail.com>
2021-05-18 16:54:46 +0300

    new ray triangle intersection test

--- src/camera/RayHit.java
+++ src/camera/RayHit.java
@@ -3,9 +3,6 @@ package camera;
 import math.vectors.Vector3D;
 import object.Triangle;
 
-/**
- * 
- */
 public class RayHit {
 	private Vector3D position;
 	private Vector3D normal;
@@ -28,40 +25,46 @@ public class RayHit {
 	}
 	
 	/**
-	 * Tests if a given ray intersects a given triangle. 
+	 * Tests if a given ray intersects a given triangle.
+	 * Implemantation of https://courses.cs.washington.edu/courses/csep557/10au/lectures/triangle_intersection.pdf
 	 * @param ray Triangle to test intersection for.
 	 * @param triangle Triangle to test intersection with.
 	 * @return RayHit object if ray and triangle intersect, NULL otherwise.
 	 */
 	public static RayHit rayTriangleIntersect(Ray ray, Triangle triangle){
-		final double EPSILON = 0.0000001;
-		Vector3D edge1, edge2, h, s, q;
-		double a, f, u, v;
+		final double EPSILON = 1e-6;
 		
-		edge1 = (Vector3D)triangle.vert1().subtract(triangle.vert0());
-		edge2 = (Vector3D)triangle.vert2().subtract(triangle.vert0());
-		h = ray.direction().cross(edge2);
-		a = edge1.dot(h);
+		//ray is parallel. Zero or infinite intersectoions.
+		if (triangle.normal().dot(ray.direction()) < EPSILON) return null;
 		
-		if (a > -EPSILON && a < EPSILON) return null; // Ray is parallel to triangle. Intersection not possible.
+		//distance of plane intersection from ray origin in direction of ray
+		double distance = (triangle.normal().dot(triangle.vert0()) - triangle.normal().dot(ray.origin()))/(triangle.normal().dot(ray.direction()));
 		
-		f = 1/a;
-		s = (Vector3D)ray.origin().subtract(triangle.vert0());
-		u = f * s.dot(h);
+		//ray plane intersection point
+		Vector3D position = new Vector3D(ray.origin().add(ray.direction().multiply(distance)));
 		
-		if (u < 0 || u > 1) return null;
+		//check if ray plane intersection is inside triangle
+		if(
+			Vector3D.cross(
+				triangle.vert1().subtract(triangle.vert0()),
+				position.subtract(triangle.vert0())
+			).dot(triangle.normal()) >= 0
+			&&
+			Vector3D.cross(
+				triangle.vert2().subtract(triangle.vert1()),
+				position.subtract(triangle.vert1())
+			).dot(triangle.normal()) >= 0
+			&&
+			Vector3D.cross(
+				triangle.vert0().subtract(triangle.vert2()),
+				position.subtract(triangle.vert2())
+			).dot(triangle.normal()) >= 0
+			
+		){
+			return new RayHit(position, triangle.normal(), distance);
+		}
 		
-		q = s.cross(edge1);
-		v = f * ray.direction().dot(q);
-		
-		if (v < 0 || u + v > 1) return null;
-		
-		double distance = f * edge2.dot(q);
-		
-		Vector3D position = (Vector3D) ray.origin().add(ray.direction().multiply(distance));
-		
-		if (distance > EPSILON) return new RayHit(position, triangle.normal(), distance);
-		
+		//intersection is outside triangle
 		return null;
 	}
 }
