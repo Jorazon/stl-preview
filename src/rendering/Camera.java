4cb27d6cc3948980ce036fdd101c70dde4b43364
Jorazon <oskari.pahkala@gmail.com>
2021-05-21 21:32:41 +0300

    Camera constructors added

--- src/rendering/Camera.java
+++ src/rendering/Camera.java
@@ -13,8 +13,36 @@ public class Camera {
 	
 	private int width;
 	private int height;
-	private double fov;
-	private FOVtype fovType;
+	private double fovHorizontal;
+	private double fovVertical;
 	
+	public Camera(int width, int height){
+		this(width, height, 90);
+	}
+	public Camera(int width, int height, double fov){
+		this(width, height, fov, FOVtype.HORIZONTAL);
+	}
+	public Camera(int width, int height, double fov, FOVtype fovType){
+		this.width = width;
+		this.height = height;
+		
+		switch (fovType) {
+			case HORIZONTAL:{
+				this.fovHorizontal = fov;
+				this.fovVertical = fov/aspect();
+			}break;
+			case VERTICAL:{
+				this.fovHorizontal = fov;
+				this.fovVertical = fov/aspect();
+			}break;
+			case DIAGONAL:{
+				this.fovHorizontal = Math.sqrt(fov*fov - width*width);
+				this.fovVertical = Math.sqrt(fov*fov - height*height);
+			}break;
+		}
+	}
 	
+	public double aspect(){
+		return width/(double)height;
+	}
 }
